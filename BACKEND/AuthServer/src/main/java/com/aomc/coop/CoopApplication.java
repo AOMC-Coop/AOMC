51da73f21d26fce5c52bd7819bdd24de8d701df7
leeyunjea <yunjea0312@naver.com>
2019-01-14 15:02:24 +0900

    Update AuthServer

---
+++ BACKEND/AuthServer/src/main/java/com/aomc/coop/CoopApplication.java
@@ -0,0 +1,14 @@
+package com.aomc.coop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopApplication.class, args);
+	}
+
+}
+

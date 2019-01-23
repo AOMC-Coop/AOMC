6e8ad2817825232d15fd47f460a06f9de2d887af
leeyunjea <yunjea0312@naver.com>
2019-01-23 18:38:37 +0900

    Update Client

--- FRONTEND/CoopFrontView/src/components/Content.vue
+++ FRONTEND/CoopFrontView/src/components/Content.vue
@@ -1,8 +1,17 @@
 <template>
 <v-content>
       <v-container fluid fill-height>
-        <v-layout justify-center align-center>
-          <v-tooltip right>
+        <v-layout justify-center align-end>
+
+          <v-flex xs12>
+            <v-text-field
+              label="Outline"
+              placeholder="대화를 입력하세요."
+              outline
+            ></v-text-field>
+          </v-flex>
+
+          <!-- <v-tooltip right>
             <v-btn
               slot="activator"
               :href="source"
@@ -13,13 +22,13 @@
               <v-icon large>code</v-icon>
             </v-btn>
             <span>Source</span>
-          </v-tooltip>
-          <v-tooltip right>
+          </v-tooltip> -->
+          <!-- <v-tooltip right>
             <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/EQOYVV" target="_blank">
               <v-icon large>mdi-codepen</v-icon>
             </v-btn>
             <span>Codepen</span>
-          </v-tooltip>
+          </v-tooltip> -->
         </v-layout>
       </v-container>
     </v-content>

644bb57c2b4814adad89b2b1006189f4650f4a52
leeyunjea <yunjea0312@naver.com>
2019-01-31 18:04:17 +0900

    Update RabbitMQ able scale out

--- BACKEND/ChattingServer/src/main/java/com/aomc/coop/controller/MessageController.java
+++ BACKEND/ChattingServer/src/main/java/com/aomc/coop/controller/MessageController.java
@@ -1,24 +1,17 @@
 package com.aomc.coop.controller;
 
 import com.aomc.coop.config.RabbitMQConfig;
-import com.aomc.coop.model.Channel;
 import com.aomc.coop.model.Message;
 import com.aomc.coop.util.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -27,8 +20,8 @@ public class MessageController {
     @Resource(name="redisTemplate")
     private ListOperations<String, Message> listOperations;
 
-    @Resource(name="redisTemplate")
-    private HashOperations<String, Object, Object> hashOperations;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
@@ -37,21 +30,12 @@ public class MessageController {
 
         //redis에 저장하기
         listOperations.rightPush(RedisUtil.redisKey, map.get("msg"));
-        //listOperations.getOperations().expire(redis_key, 5L, TimeUnit.MINUTES);
 
-        // test -> 조회해보기
-//        List<Message> messages = (ArrayList<Message>)listOperations.leftPop(redis_key);
-//        List<Message> messages = new ArrayList<>();
-//        while(true) {
-//            Message message = (Message)listOperations.leftPop(RedisUtil.redisKey);
-//            if(message == null) {
-//                break;
-//            }
-//            messages.add(message);
-//            System.out.println("Message's content = " + message.getContent());
-//        }
-
-//        LinkedHashMap<String, Message> map2 = (LinkedHashMap<String, Message>)listOperations.leftPop(RedisUtil.redisKey);
+        //receive 큐에보냄
+        if(map.get("msg") != null) {
+            rabbitTemplate.convertAndSend(RabbitMQConfig.RECEIVE_QUEUE_NAME, map.get("msg"));
+            System.out.println("receive_rabbitMQ send");
+        }
 
 
     }

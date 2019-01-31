0ed5fcbf86f92c597868e998f3651eb66717db67
dmsal325 <dmsal7325@naver.com>
2019-01-31 18:21:10 +0900

    Update Infinite Scroll

--- FRONTEND/CoopFrontView/src/components/chat/MessageList.vue
+++ FRONTEND/CoopFrontView/src/components/chat/MessageList.vue
@@ -1,24 +1,15 @@
 <template>
-<div>
-
-<!-- <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10"> -->
-
-<div infinite-wrapper>
-  <div id="msg-list" style="overflow: auto;" ref="scrollWrapper">
-  
-   <v-list class="card">
+<div class="div" infinite-wrapper  style="overflow: auto;">
     <v-card-title>
       <v-icon large left>#</v-icon>
       <span class="title font-weight-light">{{this.$store.state.channelInfo.channelName}}</span>
     </v-card-title>
-          <infinite-loading direction="top" @infinite="infiniteHandler" spinner="waveDots"></infinite-loading>
-      <!-- <infinite-loading direction="top" :on-infinite="onInfinite" ref="infiniteLoading"
-      spinner="waveDots"></infinite-loading> -->
+    <infinite-loading direction="top" @infinite="infiniteHandler" spinner="waveDots" v-if="flag" force-use-infinite-wrapper="true"></infinite-loading>
 
+   <v-list class="card">   
       <div v-for="(item,index) in getReceivedMessages" v-bind:key="index">
         <v-divider v-if="item.send_date" :key="index" inset ></v-divider>
         <v-subheader v-if="item.send_date" :key="item.send_date">{{ item.send_date }}</v-subheader>
-    
         <!-- <v-list-tile> -->
           <v-card-actions>
             <v-avatar size="42px" class="mr-3">
@@ -37,8 +28,6 @@
         </v-card-actions>
       </div>
   </v-list>
-
-
 
     <!-- <v-list subheader three-line>
      <transition-group name="list">
@@ -63,8 +52,6 @@
       </div>
     </transition-group> 
   </v-list> -->
-  </div>
-</div>
 
 </div>
 
@@ -74,128 +61,68 @@
 <script>
 import axios from "axios";
 import { mapGetters } from 'vuex'
-import InfiniteLoading from 'vue-infinite-loading';
 
 export default {
   name: 'MessageList',
-  // components: {
-  //   InfiniteLoading,
-  // },
    data() {
     return {
       start:0,
-      height: 0,
-      scrollingEnabled: false
+      flag:true
     };
   },
-   mounted() {
-     
-    this.loadInitial()
-		// this.container.scrollTop = this.container.scrollHeight;
-    // this.scrollingEnabled = true;
-		// this.$nextTick(() => {
-      // this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded');
-    // });
-      
-      
-	},
+  computed:{
+        ...mapGetters([
+      'getReceivedMessages'
+    ])
+    },
   methods: {
-    loadInitial(){
-      this.$store.state.received_messages=''
+    infiniteHandler($state) {
+      // alert('hello')
 
+      debugger
       axios.get("http://localhost:8083/api/channel/message?channelIdx=" + 8, {
         params: {
           start: this.start
         },
       }).then((response) => {
         if (response.data.status==200) {
-          
+          debugger
           this.start += 10;
-          var result = response.data.data.reverse();
-
-          this.$store.state.received_messages = result
-
+          var result = response.data.data;
+          for(var i=0;i<result.length;i++){
+              this.$store.state.received_messages.unshift(result[i]);
+          }
+          if(result.length<10){
+            $state.complete();
+          }
+          $state.loaded();
         } else {
-          alert(response.data.message)
+          $state.complete();
         }
       }); 
 
-    },
-    infiniteHandler($state) {
-    // onInfinite() {
-      // alert('hello')
-       debugger
-       setTimeout(() => {
-
-          debugger
-          axios.get("http://localhost:8083/api/channel/message?channelIdx=" + 8, {
-            params: {
-              start: this.start
-            },
-          }).then((response) => {
-            if (response.data.status==200) {
-              debugger
-              this.start += 10;
-              var result = response.data.data;
-              for(var i=0;i<result.length;i++){
-                  this.$store.state.received_messages.unshift(result[i]);
-              }
-
-              // this.height = this.container.scrollHeight
-              this.height = this.$refs.scrollWrapper.scrollHeight
-
-              if(result.length<10){
-                $state.complete();
-                //  this.$refs.infiniteLoading.$emit('$InfiniteLoading:complete');
-              }
-              $state.loaded();
-              // this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded');
-            } else {
-              $state.complete();
-              // this.$refs.infiniteLoading.$emit('$InfiniteLoading:complete');
-
-            }
-            }, 1000);
-        // // // //
-      }); 
-
     }
   },
-
-  watch: {
-    
-    height: function (newVal, oldVal) {
-      var diff = newVal - oldVal
-      // this.container.scrollTop = diff
-      debugger
-      this.$refs.scrollWrapper.scrollTop = diff
-    }
-  },
- 
   created() {
-    
+    this.$store.state.received_messages=''
 
-
-
-
-
+    axios.get("http://localhost:8083/api/channel/message?channelIdx=" + 8, {
+      params: {
+        start: this.start
+      },
+    }).then((response) => {
+      if (response.data.status==200) {
+        this.start += 10;
+        var result = response.data.data.reverse();
+        this.$store.state.received_messages = result          
+        } else {
+          alert(response.data.message)
+        }
+      });
   },
-
-    computed:{
-        ...mapGetters([
-      'getReceivedMessages'
-    ]),
-    container() {
-			return this.$el.querySelector('#msg-list');
-		},
-    }
-    
      
 };
 
-//https://github.com/yelianjie/nbbp2.0/blob/a46e27b6b6c7b4789c2487c71f32ff02d0cd98fd/src/components/HelloWorld.vue
-
-//https://github.com/milesrout/irc-log-viewer/blob/3a9d28f0e048d332845dfb57d7c1de0a873763d2/www/index.html
 </script>
 
 <style>
@@ -214,4 +141,8 @@ export default {
 .card{
   padding-left: 2%;
 }
+.div{
+   overflow-y: scroll;
+}
+
 </style>

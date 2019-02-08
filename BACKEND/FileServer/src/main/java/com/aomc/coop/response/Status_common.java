ce222707308945a8a2de652dc7c9cca4e1a324e0
Garam <Starever222@gmail.com>
2019-02-08 16:28:53 +0900

    upload & download test done, response status added

---
+++ BACKEND/FileServer/src/main/java/com/aomc/coop/response/Status_common.java
@@ -0,0 +1,18 @@
+package com.aomc.coop.response;
+
+public enum Status_common {
+
+    BAD_REQUEST("400"),
+    INTERNAL_SERVER_ERROR("500");
+
+    final private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    private Status_common(String status) {
+        this.status = status;
+    }
+
+}

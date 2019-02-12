797e7aebf59b2d7e7f731667081a63a4ae35e36f
dmsal325 <dmsal7325@naver.com>
2019-02-12 15:29:27 +0900

    Complete ChannelUser

--- FRONTEND/CoopFrontView/src/components/chat/MessageHeader.vue
+++ FRONTEND/CoopFrontView/src/components/chat/MessageHeader.vue
@@ -71,57 +71,40 @@
 
     <v-dialog v-model="createTeamDialog" width="800px" id="chat">
       <v-card>
-        <v-card-title
-          class="grey lighten-4 py-4 title"
-        >
+        <v-card-title class="primary py-4 title white--text">
         # {{this.$store.state.channelInfo.channelName}} users
         </v-card-title>
-        <v-container grid-list-sm class="pa-4">
-          <v-layout row wrap>
-            <v-flex xs12 align-center justify-space-between>
-              <v-layout align-center>
 
-                <v-list-tile
-                  v-for="(child, i) in teamMembers"
-                  :key="i"           
-                >
-            <v-flex xs12 align-center justify-space-between>
-            <v-layout align-center>
-              <v-list-tile-action >
-                <v-avatar size="42px" class="mr-3">
-                  <img
-                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                    alt=""
-                  >
+         <v-container grid-list-md text-xs-center>
+            <v-layout row wrap justify-center>
+
+              <v-flex v-for="(child, i) in this.$store.state.channelUsers" :key="`3${i}`" xs3>
+                <v-list>
+                  <v-avatar size="42px" class="mr-3">
+                  <img src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png">
                 </v-avatar>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title justify-space-between>
-                  {{ child.nickname }} 
-                </v-list-tile-title>
-              </v-list-tile-content>
-              <v-list-tile-action>
-              <!-- <v-icon right fab @click="clickInviteUserInChannel(child.idx, child.nickname)">add</v-icon> -->
-            </v-list-tile-action>
-            
-            </v-layout>
+                  <v-list-content class="px-0">{{ child.nickname }}</v-list-content>
+                </v-list>
+              </v-flex>
+          </v-layout>
+          
+          <v-layout row justify-center>
+            <v-flex xs2>
+              <v-btn @click="inviteChannel" v-if="this.$store.state.generalFlag">
+                    <v-avatar size="30px" tile>
+                    <img src="./../../assets/image/invite.png" alt="Vuetify">
+                    </v-avatar>
+                    <span>Invite Others</span>
+                  </v-btn>
             </v-flex>
-            </v-list-tile>
-
-                
-              </v-layout>
-            </v-flex>
-            
-            
-
           </v-layout>
         </v-container>
         
+       
+        
         <v-card-actions>
-          <!-- <v-btn flat color="primary">More</v-btn> -->
           <v-spacer></v-spacer>
           <v-btn flat color="primary" @click="createTeamDialog = !createTeamDialog">Cancel</v-btn>
-          <!-- <v-btn flat @click="saveTeam">Save</v-btn> -->
         </v-card-actions>
       </v-card>
     </v-dialog>

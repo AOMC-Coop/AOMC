3839e40ab4d5e9d2bbc9e5d3e7d6bc9d530ef331
Garam <Starever222@gmail.com>
2019-02-14 18:46:30 +0900

    change email auth sending

--- BACKEND/AuthServer/src/main/java/com/aomc/coop/utils/mail/MailSend.java
+++ BACKEND/AuthServer/src/main/java/com/aomc/coop/utils/mail/MailSend.java
@@ -25,23 +25,23 @@ public class MailSend {
 
     }
 
-    public void mailsendForMissingPwd(JavaMailSender mailSender, String uid, int idx){
-
-        try{
-            String url = "http://localhost:8082/members/pwd/" + idx;
-            MailConfig sendMail = new MailConfig(mailSender);
-            sendMail.setFrom("Starever222@gmail.com", "CoopDeveloper");
-            sendMail.setTo(uid);
-            sendMail.setSubject("[Let's change password!]");
-            sendMail.setText(new StringBuffer()
-                    .append("(<b style=\"text-decoration:none\">Click link below to change password!</b>) <br/>")
-                    .append("<a href="+ url + " role='button'>CLICK</a>")
-                    .toString());
-            sendMail.send();
-
-        }catch (Exception e){
-// ***** 시간 남으면 에러 처리 할 것
-        }
-
-    }
+//    public void mailsendForMissingPwd(JavaMailSender mailSender, String uid, int idx){
+//
+//        try{
+//            String url = "http://localhost:8082/members/pwd/" + idx;
+//            MailConfig sendMail = new MailConfig(mailSender);
+//            sendMail.setFrom("Starever222@gmail.com", "CoopDeveloper");
+//            sendMail.setTo(uid);
+//            sendMail.setSubject("[Let's change password!]");
+//            sendMail.setText(new StringBuffer()
+//                    .append("(<b style=\"text-decoration:none\">Click link below to change password!</b>) <br/>")
+//                    .append("<a href="+ url + " role='button'>CLICK</a>")
+//                    .toString());
+//            sendMail.send();
+//
+//        }catch (Exception e){
+//// ***** 시간 남으면 에러 처리 할 것
+//        }
+//
+//    }
 }

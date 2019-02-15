8fa2d977a587fe2abc56ed01c4e3e4abb4602feb
Garam <Starever222@gmail.com>
2019-02-15 15:50:41 +0900

    finish linking vue with file server and chatting server

--- FRONTEND/CoopFrontView/src/components/chat/MessageForm.vue
+++ FRONTEND/CoopFrontView/src/components/chat/MessageForm.vue
@@ -1,26 +1,41 @@
 <template>
+
  <v-container fluid fill-height>
+    <!-- ***** 추후 대체 되어야 할 파일 업로드 버튼. vue로 바꾸어보자 -->
+        <div class="container">
+          <div class="large-12 medium-12 small-12 cell">
+            <label>File
+              <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+            </label>
+            <button v-on:click="submitFile()">Submit</button>
+          </div>
+        </div>
+
         <v-layout justify-center align-end>
 
           <v-flex xs12>
-            <v-textarea class="area"
-            :rules="[(v) => v.length <= 500 || 'Max 500 characters']"
-            :counter = 500
-            placeholder="대화를 입력하세요."
-            v-model="msg"
-            outline
-            @keypress.13.prevent="submitMessageFunc"
-            prepend-icon="add"
-            clearable
-            ></v-textarea>
-            <upload-btn icon>
-              <template slot="icon">
-                <v-icon>add</v-icon>
-              </template>
-            </upload-btn>
+
+              <!-- <form @submit.prevent="sendFile"> 
+                <upload-btn icon>
+                  <template slot="icon">
+                    <v-icon>add</v-icon>
+                  </template>
+                </upload-btn>
+              </form> -->
+
+              <v-textarea class="area"
+              :rules="[(v) => v.length <= 500 || 'Max 500 characters']"
+              :counter = 500
+              placeholder="대화를 입력하세요."
+              v-model="msg"
+              outline
+              @keypress.13.prevent="submitMessageFunc"
+              clearable
+              ></v-textarea>
+
           </v-flex>
         </v-layout>
-      </v-container>
+  </v-container>
 
 </template>
 
@@ -28,7 +43,10 @@
 // import SockJS from "sockjs-client";
 // import Stomp from "webstomp-client";
 import moment from 'moment'
+import UploadButton from 'vuetify-upload-button';
+import axios from "axios";
 
+  
 var now = new moment();
 var send_date = now.format("dddd, MMMM Do").toString()
 var send_time = now.format("LT").toString()
@@ -36,7 +54,11 @@ var send_db_date = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
 
 
 export default {
+
   name: 'MessageForm',
+  components: {
+      'upload-btn': UploadButton
+  },
   data() {
     return {
       msg: {
@@ -49,6 +71,7 @@ export default {
       channel: {
         idx: this.$store.state.channelInfo.idx
       },
+      file: '',
       userIdx: localStorage.getItem("userIdx"),
       userNickName: localStorage.getItem("userNickName")
     }
@@ -79,19 +102,57 @@ export default {
 
       // return true;
     },
+    submitFile(){
+      // Initialize the form data
+      let formData = new FormData();
+      // Add the form data we need to submit
+      let channel_idx = this.$store.state.channelInfo.idx
+      let url = this.$store.state.ip + ":8085/1"// expected to be "localhost:8085/1"
 
-    // disconnect() {
-    //   if (this.stompClient) {
-    //     this.stompClient.disconnect();
-    //   }
-    //   this.connected = false;
-    // },
-    // tickleConnection() {
-    //   this.connected ? this.disconnect() : this.connect();
-    // }
-  },
-
-
+      const sendMessage = { 
+        content: this.msg , 
+        channel_idx: this.$store.state.channelInfo.idx, 
+        user_idx: this.userIdx, 
+        nickname: this.userNickName, 
+        send_date: send_date, 
+        send_time:send_time, 
+        send_db_date: send_db_date
+      };//레디스에서 받은 사용자의 nickname을 세팅
+      // this.msg.nickname = 'yunyun',
+      // this.msg.send_date
+      // this.msg.send_time
+      // this.msg.send_db_date    
+      // "channel_idx":1,
+      // "user_idx":1
+      formData.append('file', this.file);
+      formData.append('message', JSON.stringify(sendMessage));
+      debugger
+      alert(JSON.stringify(sendMessage))
+ 
+      // formData.append('message', sendMessage);
+      // Make the request to the POST /single-file URL
+      
+      
+      
+      axios.post( url,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }).then(function(){
+          console.log('Successfully submitted file!');
+        })
+        .catch(function(){
+          console.log('Fail to submit file!');
+        });
+      },
+      // Handles a change on the file upload
+      handleFileUpload(){
+        this.file = this.$refs.file.files[0];
+        debugger
+      }
+    },
 
    created() {
        this.msg = '';
@@ -135,9 +196,22 @@ export default {
     }
 
 };
+
+    // disconnect() {
+    //   if (this.stompClient) {
+    //     this.stompClient.disconnect();
+    //   }
+    //   this.connected = false;
+    // },
+    // tickleConnection() {
+    //   this.connected ? this.disconnect() : this.connect();
+    // }
 </script>
 <style>
 .area {
   padding-top: 50
+}
+.left_align{
+  float: left;
 }
 </style>

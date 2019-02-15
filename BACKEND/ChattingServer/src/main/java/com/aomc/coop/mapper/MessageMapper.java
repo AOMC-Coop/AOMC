3494319c7539b089e8444259c685960abd08a3e3
leeyunjea <yunjea0312@naver.com>
2019-02-15 16:50:41 +0900

    Update FileServer and ChattingServer redis to mysql

--- BACKEND/ChattingServer/src/main/java/com/aomc/coop/mapper/MessageMapper.java
+++ BACKEND/ChattingServer/src/main/java/com/aomc/coop/mapper/MessageMapper.java
@@ -16,12 +16,12 @@ public interface MessageMapper {
     int createMessage(final Message message, final int channelIdx, final int userIdx);
 
     //file 테이블
-    @Insert("INSERT INTO file(content) VALUES(#{file.content})")
-    @Options(useGeneratedKeys = true, keyProperty = "idx")
-    int createFile(File file);
+    @Insert("INSERT INTO file(content) VALUES(#{fileUrl})")
+    @Options(useGeneratedKeys = true, keyProperty = "file.idx")
+    int createFile(File file, String fileUrl);
 
     //message with file_url 테이블
-    @Insert("INSERT INTO messages(message_idx, content, channel_idx, user_idx, file_idx) VALUES(#{message.message_idx}, #{message.content}, #{channelIdx}, #{userIdx}, #{fileIdx})")
+    @Insert("INSERT INTO messages(message_idx, content, channel_idx, user_idx, file_idx) VALUES(#{message.message_idx}, #{message.content}, #{message.channel_idx}, #{message.user_idx}, #{fileIdx})")
     void createMessageWithFile(Message message, int fileIdx);
 
 }

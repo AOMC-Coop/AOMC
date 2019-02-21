0ca0310b2afc1cd55b7c8ec198faad06120df533
Garam <Starever222@gmail.com>
2019-02-21 17:22:39 +0900

    Add filter & interceptor to file server

---
+++ BACKEND/AuthServer/src/main/java/com/aomc/coop/service/TokenService.java
@@ -0,0 +1,105 @@
+package com.aomc.coop.service;
+
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.auth0.jwt.JWT.require;
+
+@Slf4j
+@Service
+public class TokenService {
+
+    @Value("${JWT.ISSUER}")
+    private String ISSUER;
+
+    @Value("${JWT.SECRET}")
+    private String SECRET;
+
+    @Resource(name = "redisTemplate")
+    private HashOperations<String, String, String> values;
+
+
+    public boolean isUsableToken(String token) {
+
+        String idx = (String) values.get(token, "idx");
+
+        if(idx == null ) return false;
+        else return true;
+
+    }
+
+
+    /**
+     * 토큰 해독
+     *
+     * @param token 토큰
+     * @return 로그인한 사용자의 회원 고유 IDX
+     */
+
+    public TokenService.Token decode(final String token) {
+
+        try {
+            //토큰 해독 객체 생성
+            final JWTVerifier jwtVerifier = require(Algorithm.HMAC256(SECRET)).withIssuer(ISSUER).build();
+            //토큰 검증
+            DecodedJWT decodedJWT = jwtVerifier.verify(token);
+
+            //토큰 payload 반환, 정상적인 토큰이라면 토큰 주인(사용자) 고유 ID, 아니라면 -1
+            return new TokenService.Token(decodedJWT.getClaim("user_idx").asLong().intValue());
+        } catch (JWTVerificationException jve) {
+            log.error(jve.getMessage());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return new TokenService.Token();
+    }
+
+
+    public static class Token {
+        //토큰에 담길 정보 필드
+        //초기값을 -1로 설정함으로써 로그인 실패시 -1반환
+        private int user_idx = -1;
+
+        public Token() {
+        }
+
+        public Token(final int user_idx) {
+            this.user_idx = user_idx;
+        }
+
+        public int getUser_idx() {
+            return user_idx;
+        }
+
+    }
+
+    //반환될 토큰Res
+    public static class TokenRes {
+        //실제 토큰
+        private String token;
+
+        public TokenRes() {
+        }
+
+        public TokenRes(final String token) {
+            this.token = token;
+        }
+
+        public String getToken() {
+            return token;
+        }
+
+        public void setToken(String token) {
+            this.token = token;
+        }
+
+    }
+}

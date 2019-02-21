6fa765f1e375f231352e74134ba2f8b932728d4b
dmsal325 <dmsal7325@naver.com>
2019-02-21 14:15:17 +0900

    TOMCAT

--- BACKEND/ChatApiServer/src/main/java/com/aomc/coop/CoopChattingapiserverApplication.java
+++ BACKEND/ChatApiServer/src/main/java/com/aomc/coop/CoopChattingapiserverApplication.java
@@ -9,11 +9,19 @@ import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class CoopChattingapiserverApplication {
+public class CoopChattingapiserverApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CoopChattingapiserverApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(CoopChattingapiserverApplication.class, args);

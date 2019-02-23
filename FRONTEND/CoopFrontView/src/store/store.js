79fba946f2d8596ddbeb24762b88f66082ad427f
dmsal325 <dmsal7325@naver.com>
2019-02-23 15:00:22 +0900

    commit

--- FRONTEND/CoopFrontView/src/store/store.js
+++ FRONTEND/CoopFrontView/src/store/store.js
@@ -7,7 +7,7 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
   // 메세지 를 state 속성으로 추가
   state: {
-    ip:"http://10.240.202.225",
+    ip:"http://localhost",
     stompClient:null,
     stompSubscription: null,
     connected:false,

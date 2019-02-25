b6b15320b271fae42403566f13d8dbb21dbe999c
leeyunjea <yunjea0312@naver.com>
2019-02-25 19:09:53 +0900

    Update pom.xml

--- BACKEND/AuthServer/src/main/java/com/aomc/coop/service/MemberService.java
+++ BACKEND/AuthServer/src/main/java/com/aomc/coop/service/MemberService.java
@@ -21,6 +21,7 @@ import com.aomc.coop.utils.ResponseType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;

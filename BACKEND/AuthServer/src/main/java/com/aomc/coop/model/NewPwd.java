c98429da58196682729605cd58cb6931a644c591
leeyunjea <yunjea0312@naver.com>
2019-02-25 17:36:01 +0900

    Update maven war

--- BACKEND/AuthServer/src/main/java/com/aomc/coop/model/NewPwd.java
+++ BACKEND/AuthServer/src/main/java/com/aomc/coop/model/NewPwd.java
@@ -1,6 +1,7 @@
 package com.aomc.coop.model;
 
 import lombok.Data;
+
 import java.io.Serializable;
 
 @Data
--- BACKEND/ChatApiServer/src/main/java/com/aomc/coop/utils/redis/RedisUtil.java
+++ BACKEND/ChatApiServer/src/main/java/com/aomc/coop/utils/redis/RedisUtil.java
@@ -2,5 +2,7 @@ package com.aomc.coop.utils.redis;
 
 public class RedisUtil {
 
+//    public static String redisKey = "channelIdx:channel";
       public static String redisKey = "channelIdx:";
+      public static String ChannelInfoKey = "channelIdx:channelInfo";
 }

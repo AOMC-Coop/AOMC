c54fb1ee7e4fd40441e3492e66c118935ae64b02
Garam <Starever222@gmail.com>
2019-09-29 16:59:22 +0900

    Status 코드 업데이트

--- BACKEND/AuthServer/src/main/java/com/aomc/coop/response/Status_3000.java
+++ BACKEND/AuthServer/src/main/java/com/aomc/coop/response/Status_3000.java
@@ -1,39 +1,37 @@
 package com.aomc.coop.response;
 
 public enum Status_3000 {
-// *** 다 끝나고 나서 한 번 손보긴 해야함 -> 더욱 자세히 case 나누기
     SUCCESS_Register("3201"),
-    SUCCESS_Register_Auth_Mail_Sent("3210"),
+    SUCCESS_Register_Auth_Mail_Sent("3202"),
     FAIL_Register("3401"),
     FAIL_Register_Duplicate("3402"),
-    FAIL_Register_Timeout("3412"),
+    FAIL_Register_Timeout("3403"),
 
-    SUCCESS_Withdrawal("3202"),
-    FAIL_Withdrawal("3403"),
+    SUCCESS_Withdrawal("3210"),
+    FAIL_Withdrawal("3410"),
 
-    SUCCESS_Login("3203"),
-    FAIL_Login("3404"),
-    FAIL_Login_Wrong_ID("3405"),
-    FAIL_Login_Wrong_Password("3406"),
+    SUCCESS_Login("3220"),
+    FAIL_Login("3420"),
+    FAIL_Login_Wrong_ID("3421"),
+    FAIL_Login_Wrong_Password("3422"),
+    FAIL_Login_Withdrawal("3423"),
 
-    SUCCESS_Logout("3204"),
-    FAIL_Logout("3407"),
+    SUCCESS_Logout("3230"),
+    FAIL_Logout("3430"),
 
-    SUCCESS_Get_Profile("3205"),
-    FAIL_Get_Profile("3408"),
-    FAIL_Get_Profile_Wrong_Idx("3409"),
+    SUCCESS_Get_Profile("3240"),
+    FAIL_Get_Profile("3440"),
+    FAIL_Get_Profile_Wrong_Idx("3441"),
 
-    SUCCESS_Set_Profile("3206"),
-    FAIL_Set_Profile("3410"),
-    FAIL_Set_Profile_Wrong_Idx("3411"),
+    SUCCESS_Set_Profile("3250"),
+    FAIL_Set_Profile("3450"),
+    FAIL_Set_Profile_Wrong_Idx("3451"),
 
-    SUCCESS_Change_Pwd("3207"),
-    FAIL_Change_Pwd("3413"),
+    SUCCESS_Change_Pwd("3260"),
+    FAIL_Change_Pwd("3460"),
 
-    SUCCESS_Missing_Pwd_Email_Auth("3211"),
-    FAIL_Missing_Pwd_Email_Auth("3414");
-
-
+    SUCCESS_Missing_Pwd_Email_Auth("3270"),
+    FAIL_Missing_Pwd_Email_Auth("3470");
 
     final private String status;
 
